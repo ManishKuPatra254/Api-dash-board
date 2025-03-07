1f1ccb5ede43ac4058f97f8083d8785df7de09d8
Manish <manishpatra254@gmail.com>
2025-03-08 00:57:41 +0530

    added the chakra ui and all ui and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react/jsx-runtime";
 import {
-  Button,
+  Box,
   Card,
   CardBody,
   CardFooter,
@@ -8,37 +8,42 @@ import {
   HStack,
 } from "@chakra-ui/react";
 import { Avatar } from "./components/ui/avatar";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <Fragment>
-      <HStack style={{ padding: "10px" }}>
-        <Button style={{ padding: "10px" }}>Click me</Button>
-        <Button style={{ padding: "10px" }}>Click me</Button>
+      <HStack wrap="wrap" gap="6" p={3}>
+        <Button variant="solid">Solid</Button>
+        <Button variant="subtle">Subtle</Button>
+        <Button variant="surface">Surface</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="ghost">Ghost</Button>
+        <Button variant="plain">Plain</Button>
       </HStack>
 
-      <CardRoot width="320px" style={{ padding: "10px" }}>
-        <CardBody gap={2}>
-          <Avatar
-            src="https://picsum.photos/200/300"
-            name="Nue Camp"
-            size="lg"
-            shape="rounded"
-          />
-          <Card.Title mt="2">Nue Camp</Card.Title>
-          <Card.Description>
-            This is the card body. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
-            Curabitur nec odio vel dui euismod fermentum.
-          </Card.Description>
-        </CardBody>
-        <CardFooter justifyContent="flex-end">
-          <Button variant="outline" style={{ padding: "10px" }}>
-            View
-          </Button>
-          <Button style={{ padding: "10px" }}>Join</Button>
-        </CardFooter>
-      </CardRoot>
+      <Box p={3}>
+        <CardRoot width="320px" p={3}>
+          <CardBody gap={2}>
+            <Avatar
+              src="https://picsum.photos/200/300"
+              name="Nue Camp"
+              size="lg"
+              shape="rounded"
+            />
+            <Card.Title mt="2">Nue Camp</Card.Title>
+            <Card.Description>
+              This is the card body. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
+              Curabitur nec odio vel dui euismod fermentum.
+            </Card.Description>
+          </CardBody>
+          <CardFooter justifyContent="flex-end">
+            <Button variant="outline">View</Button>
+            <Button>Join</Button>
+          </CardFooter>
+        </CardRoot>
+      </Box>
     </Fragment>
   );
 }
