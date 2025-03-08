78a9ea6ac2c3665536ed575f4eb103641b2095db
Pawan <pawan7260kumar@gmail.com>
2025-03-08 11:58:27 +0530

    new all

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,33 @@
-import { Fragment } from "react/jsx-runtime";
+import { Button } from "@/components/ui/button"
 import {
-  Box,
   Card,
-  CardBody,
+  CardContent,
+  CardDescription,
   CardFooter,
-  CardRoot,
-  HStack,
-} from "@chakra-ui/react";
-import { Avatar } from "./components/ui/avatar";
-import { Button } from "./components/ui/button";
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function App() {
   return (
-    <Fragment>
-      <HStack wrap="wrap" gap="6" p={3}>
-        <Button variant="solid">Solid</Button>
-        <Button variant="subtle">Subtle</Button>
-        <Button variant="surface">Surface</Button>
-        <Button variant="outline">Outline</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="plain">Plain</Button>
-      </HStack>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button className="bg-foreground">Click me</Button>
 
-      <Box p={3}>
-        <CardRoot width="320px" p={3}>
-          <CardBody gap={2}>
-            <Avatar
-              src="https://picsum.photos/200/300"
-              name="Nue Camp"
-              size="lg"
-              shape="rounded"
-            />
-            <Card.Title mt="2">Nue Camp</Card.Title>
-            <Card.Description>
-              This is the card body. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
-              Curabitur nec odio vel dui euismod fermentum.
-            </Card.Description>
-          </CardBody>
-          <CardFooter justifyContent="flex-end">
-            <Button variant="outline">View</Button>
-            <Button>Join</Button>
-          </CardFooter>
-        </CardRoot>
-      </Box>
-    </Fragment>
-  );
+      <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-});
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
